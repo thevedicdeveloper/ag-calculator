efc0b0e1feea42a792baab4247522da999d1a7b5
The Vedic Developer <thevedicdev@gmail.com>
2023-03-23 22:15:04 -0400

    intialized ag-calculator

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
 }
 
-bootstrapApplication(App);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
